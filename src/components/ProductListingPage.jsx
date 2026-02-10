28f72f80d00f2017e483211396602e439515182d
yaredgebre <yaredgebre120@gmail.com>
2026-02-10 12:03:09 -0300

    initalization of the productlisting page

---
+++ src/components/ProductListingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProductListingPage() {
+  const products = {
+      "id": 1,
+      "title": "Essence Mascara Lash Princess",
+      "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+      "category": "beauty",
+      "price": 9.99,
+      "discountPercentage": 7.17,
+      "rating": 4.94,
+      "stock": 5,
+      "thumbnail": "...",}
+
+  return (
+    <div>ProductListingPage</div>
+  )
+}
+
+export default ProductListingPage

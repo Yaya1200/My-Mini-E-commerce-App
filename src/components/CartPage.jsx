025aa8fa2085cde927e1747770efdccd6ef8aeee
yaredgebre <yaredgebre120@gmail.com>
2026-02-11 05:19:36 -0300

    using grid to align products

---
+++ src/components/CartPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function CartPage() {
+  return (
+    <div className='grid gap-4 grid-cols-3'>
+      <h1>CartPage</h1>
+      <h1>CartPage</h1>
+      <h1>CartPage</h1>
+      <h1>CartPage</h1>
+      <h1>CartPage</h1>
+      <h1>CartPage</h1>
+      
+      
+      </div>
+  )
+}
+
+export default CartPage

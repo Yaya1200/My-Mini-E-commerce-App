cf610953f44dbd16842e3c3593e39cc211ce2a8f
yaredgebre <yaredgebre120@gmail.com>
2026-02-11 10:33:30 -0300

    adding items to product page

--- src/components/ProductListingPage.jsx
+++ src/components/ProductListingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function ProductListingPage() {
-  const products = {
+  const products =[ {
       "id": 1,
       "title": "Essence Mascara Lash Princess",
       "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
@@ -10,10 +10,50 @@ function ProductListingPage() {
       "discountPercentage": 7.17,
       "rating": 4.94,
       "stock": 5,
-      "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",}
+      "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"}]
 
   return (
-    <div>ProductListingPage</div>
+    <div>{
+      products.map((element)=>(
+        <div className='grid gap-3 grid-cols-5'>
+          <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+           <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+           <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+           <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+           <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+          <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+          <div>
+     <img src={products[0].thumbnail} alt="Phone product" />
+     <h2>{products[0].title}</h2>
+      <button>add</button>
+          </div>
+          </div>
+      ))}
+      </div>
   )
 }
 

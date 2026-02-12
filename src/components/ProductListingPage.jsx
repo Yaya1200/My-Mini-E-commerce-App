d3fc306e242bb3dd0c269e204ca080cb2f539dbd
yaredgebre <yaredgebre120@gmail.com>
2026-02-12 07:08:18 -0300

    upadating the product page with new values

--- src/components/ProductListingPage.jsx
+++ src/components/ProductListingPage.jsx
@@ -1,60 +1,28 @@
-import React from 'react'
+import { useGetProductsQuery } from "../featchers/products/productApi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../featchers/cart/cartSlice";
 
-function ProductListingPage() {
-  const products =[ {
-      "id": 1,
-      "title": "Essence Mascara Lash Princess",
-      "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-      "category": "beauty",
-      "price": 9.99,
-      "discountPercentage": 7.17,
-      "rating": 4.94,
-      "stock": 5,
-      "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"}]
+const ProductPage = () => {
+  const { data, isLoading, error } = useGetProductsQuery();
+  const dispatch = useDispatch();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading products</p>;
 
   return (
-    <div>{
-      products.map((element)=>(
-        <div className='grid gap-3 grid-cols-5'>
-          <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-           <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-           <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-           <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-           <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-          <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-          <div>
-     <img src={products[0].thumbnail} alt="Phone product" />
-     <h2>{products[0].title}</h2>
-      <button>add</button>
-          </div>
-          </div>
-      ))}
-      </div>
-  )
-}
+    <div>
+      {data.products.map((product) => (
+        <div key={product.id}>
+          <h4>{product.title}</h4>
+          <p>${product.price}</p>
 
-export default ProductListingPage
+          <button onClick={() => dispatch(addToCart(product))}>
+            Add to Cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductPage;

0df33cbedc802e64d48e51a26d86b30622e527d3
yaredgebre <yaredgebre120@gmail.com>
2026-02-13 05:42:36 -0300

    adding the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import CartPage from './components/CartPage'
 import ProductListingPage from './components/ProductListingPage'
+import Header from './components/header'
+import Footer from './components/footer'
+import { Routes, Route } from 'react-router-dom'
+import ProductDetailPage from "./components/ProductDetailPage";
 
 function App() {
-
   return (
-    
-<ProductListingPage/>
+    <>
+      <Header />
+<Routes>
+  <Route path="/" element={<ProductListingPage />} />
+  <Route path="/products/:productId" element={<ProductDetailPage />} />
+  <Route path="/cart" element={<CartPage />} />
+</Routes>
+
+      <Footer />
+    </>
   )
 }
 
 export default App
+

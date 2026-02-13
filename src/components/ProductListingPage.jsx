318ceffe6c5ff505ba0802d20937e55f73f807ac
yaredgebre <yaredgebre120@gmail.com>
2026-02-13 05:44:04 -0300

    updating the product lising page

--- src/components/ProductListingPage.jsx
+++ src/components/ProductListingPage.jsx
@@ -1,22 +1,36 @@
 import { useGetProductsQuery } from "../featchers/products/productApi";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../featchers/cart/cartSlice";
+import { Link } from "react-router-dom";
 
 const ProductPage = () => {
-  const { data, isLoading, error } = useGetProductsQuery();
+  const { data = [], isLoading, error } = useGetProductsQuery();
   const dispatch = useDispatch();
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error loading products</p>;
-
+  console.log(data)
   return (
-    <div>
-      {data.products.map((product) => (
-        <div key={product.id}>
-          <h4>{product.title}</h4>
-          <p>${product.price}</p>
+    <div className="grid grid-cols-5 gap-2 ml-5">
+      {data?.products?.map((product) => (
+        <div className="grid-content" key={product.id}>
+          <Link to={`/products/${product.id}`}>
+          <img className="images" src={product.thumbnail} alt={product.title} />
+          </Link>
+          
+          <p className="font-medium">{product.title}</p>
 
-          <button onClick={() => dispatch(addToCart(product))}>
+          <div className="container">
+            
+            product rating: {product.rating}
+          </div>
+
+          <p>product price ${(product.price)}</p>
+
+          <button
+            className="bg-blue-600 text-white px-2 py-1 rounded-md hover:bg-blue-500 transition"
+            onClick={() => dispatch(addToCart(product))}
+          >
             Add to Cart
           </button>
         </div>
